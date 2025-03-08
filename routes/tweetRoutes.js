d085a8cfc1d6b68d9bb1f51322240f74a355baeb
Sanup Parajuli <sanupparajuli1@gmail.com>
2025-03-08 23:28:38 +0545

    edit retweet

--- routes/tweetRoutes.js
+++ routes/tweetRoutes.js
@@ -7,8 +7,4 @@ const router = express.Router();
 // Routes
 router.post('/create_tweet', protect, createTweet); // Create a tweet
 router.get('/show_tweet', getAllTweets); // Get all tweets
-router.get('/:id', getTweetById); // Get a specific tweet
-router.put('/:id', protect, updateTweet); // Update a tweet
-router.delete('/:id', protect, deleteTweet); // Delete a tweet
-
-module.exports = router;
+ule.exports = router;
