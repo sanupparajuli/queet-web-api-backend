4f68a144bea9c71a316079da26923b7907a21ff8
Sanup Parajuli <sanupparajuli1@gmail.com>
2025-03-08 22:51:12 +0545

    complete user route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { registerUser, loginUser, getUserProfile, updateProfilePicture, updateUserProfile } = require('../controllers/userController');
+const { protect } = require('../middleware/authMiddleware');
+const upload = require('../middleware/uploadMiddleware'); // Import Multer
 
 const router = express.Router();
 
@@ -7,5 +9,9 @@ const router = express.Router();
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 
+// Profile Routes
+router.get('/profile', protect, getUserProfile);
+router.put('/profile-picture', protect, upload.single('profile_picture'), updateProfilePicture);
+router.put('/update_profile', protect, updateUserProfile);
 
 module.exports = router;
