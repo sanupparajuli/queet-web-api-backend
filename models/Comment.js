9e89326c9a32024c5787e692e35e4174ffabc637
Sanup Parajuli <sanupparajuli1@gmail.com>
2025-03-08 23:06:30 +0545

    add comment model

---
+++ models/Comment.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const CommentSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    tweet: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Tweet',
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Comment', CommentSchema);
