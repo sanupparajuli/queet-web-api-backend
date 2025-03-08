94244016354b4589a9e1aca3bdd2d200bca73f68
Sanup Parajuli <sanupparajuli1@gmail.com>
2025-03-08 22:52:51 +0545

    change index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const connectDB = require('./database/db');
+
+
+
+// Load environment variables
+require('dotenv').config();
+
+
+
+// Initialize Express app
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve uploaded files as static files
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+// Server Start
+const PORT = 8080;
+
+app.listen(PORT, () => {
+    console.log(` Server running on http://localhost:${PORT}`);
+});
+
